f49fcf5c27a25d893aef167e7178f08277570386
Shanta Nathwani <shanta@shanta.ca>
2019-05-04 11:12:29 -0400

    converts app.js from functional to class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Personal Project with React</h1>
-      <h2>by Shanta R. Nathwani - Cohort 7</h2>
-      <h3>Please provide the Github username that you would like to search: </h3>
-      <input type="text" />
-      <button>Submit</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Personal Project with React</h1>
+        <h2>by Shanta R. Nathwani - Cohort 7</h2>
+        <h3>Please provide the Github username that you would like to search: </h3>
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
