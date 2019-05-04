1280c39b26b774c347150c3cbc59373aef2553a5
Shanta Nathwani <shanta@shanta.ca>
2019-05-04 11:10:58 -0400

    adds basic framework for html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Personal Project with React</h1>
+      <h2>by Shanta R. Nathwani - Cohort 7</h2>
+      <h3>Please provide the Github username that you would like to search: </h3>
+      <input type="text" />
+      <button>Submit</button>
     </div>
   );
 }
