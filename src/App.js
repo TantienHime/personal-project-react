3c048606ca0571747df4f508093853d3f37384f1
Shanta Nathwani <shanta@shanta.ca>
2019-05-06 21:47:39 -0400

    comments out any unnecessary pieces for the api call

--- src/App.js
+++ src/App.js
@@ -13,24 +13,24 @@ class App extends React.Component {
     };
   }
 
-  listGithub() {
-    return this.state.myGithub.map(githubObject => <div>{githubObject.id}</div>)
-  }
+  // listGithub() {
+  //   return this.state.myGithub.map(githubObject => <div>{githubObject.id}</div>)
+  // }
   // Fetch pull requests by username: https://developer.github.com/v3/pulls/#list-pull-requests
   // example https://api.github.com/search/issues?q=author%3Atantienhime+type%3Apr = open
   // Ref: https://stackoverflow.com/questions/17412809/how-to-get-my-pull-requests-from-github-api
-  componentDidMount() {
-    fetch(githubApi)
-      .then(res => res.json())
-      .then(data => {
-        const githubResults = data.results;
+//   componentDidMount() {
+//     fetch(githubApi)
+//       .then(res => res.json())
+//       .then(data => {
+//         const githubResults = data.results;
         
-      this.setState({
-        myGithub: githubResults
-      });
-  });
-  console.log(this.state.myGithub);
-}
+//       this.setState({
+//         myGithub: githubResults
+//       });
+//   });
+//   console.log(this.state.myGithub);
+// }
   render() {
     return (
       <div>
@@ -39,7 +39,7 @@ class App extends React.Component {
           <h2>by Shanta R. Nathwani - Cohort 7</h2>
         </header>
         Your stuff here
-        {this.listGithub()}
+        {/* {this.listGithub()} */}
         <Form />
         <Forks />
         <Pulls />
